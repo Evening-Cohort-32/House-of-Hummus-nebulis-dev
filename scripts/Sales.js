9c1f0ba1250c62e4cafacfc6912dcf5de16522a6
Jaiden Rothrock <jaiden_rothrock@hotmail.com>
2026-05-30 11:55:30 -0500

    removed unecessary console log previously used for testing

--- scripts/Sales.js
+++ scripts/Sales.js
@@ -1,8 +1,6 @@
 export const Sales = async () => {
     const sales = await fetch("http://localhost:8088/purchases").then(res => res.json())
 
-    console.log("sales data:", sales)
-
     let salesDivs = sales.map(sale => `<div>Receipt #${sale.id} = $${sale.total.toFixed(2)}</div>`)
 
     salesDivs = salesDivs.join("")
